7989d79a808f70c776bce5cd6a29ddd8d920dd0e
samuel fagbohun <samuelfagbohun041@gmail.com>
2024-09-20 03:37:36 +0100

    completed styling and implementation

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import Button from './components/Button'
+import ButtonBox from './components/ButtonBox'
+import Screen from './components/Screen'
+import Wrapper from './components/Wrapper'
+import CalProvider from './context/CalcContext'
+import logo_icon from './assets/payscribe_logo.png'
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+]
+function App() {
+
+
+  return (
+    <CalProvider>
+      <div>
+         <div className='logo-wrapper'>
+          <img src={logo_icon} alt="logo-icon" />
+        </div>
+      <Wrapper>
+       
+       <Screen />
+       <ButtonBox>
+         {btnValues.flat().map((btn, i) => (
+           <Button value={btn} key={i} />
+         ))}
+       </ButtonBox>
+    
+     </Wrapper>
+      </div>
+    
+   
+    </CalProvider>
+  )
+}
+
+export default App
---
+++ src/context/CalcContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState} from 'react'
+
+export const CalcContext = createContext()
+function CalProvider({children}) {
+  const [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0
+  });
+
+  const providerValue = {
+    calc, setCalc
+  }
+  return (
+    <CalcContext.Provider value={providerValue}>
+      {children}
+    </CalcContext.Provider>
+  )
+}
+
+export default CalProvider
